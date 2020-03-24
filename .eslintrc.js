305b54e48b0f78ec8ceb621ab0e383a3e21ae273
Hugh Hoang <hugh.hoang@gmail.com>
2020-03-25 11:25:53 +1300

    chore: build and linting and formatting

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: [
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  globals: { },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
