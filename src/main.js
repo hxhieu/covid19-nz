37244351883a7a8d9e2462ba55d7fee158b59822
Hugh Hoang <hugh.hoang@gmail.com>
2020-03-25 21:44:10 +1300

    chore: attempt using Element UI with Vue3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import store from './store'
+import ElementUI from 'element-ui'
+//import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(ElementUI)
 app.use(store)
 
 app.mount('#app')
