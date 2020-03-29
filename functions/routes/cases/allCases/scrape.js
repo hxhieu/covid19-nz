51238a37dbdb84b1c809c6aaee47356c3075a0bf
Hugh Hoang <hugh.hoang@gmail.com>
2020-03-30 00:04:01 +1300

    temp: fix scraping new content

--- functions/routes/cases/allCases/scrape.js
+++ functions/routes/cases/allCases/scrape.js
@@ -6,25 +6,35 @@ const functions = require('firebase-functions')
 const { cacheFileRaw } = require('./const')
 
 const parseTable = ($, selector, index) => {
+  const parseString = str => {
+    if (!str) return 'Unknown'
+    const trim = str.trim()
+    if (!trim) return 'Unknown'
+    return trim
+  }
+  const parseDate = date => {
+    return parseString(date).replace(/\//g, '-')
+  }
+  const parseGender = gender => parseString(gender)
+  const parseAge = age => parseString(age)
+  const parseLocation = location => parseString(location)
+  const parseOverseas = overseas => parseString(overseas)
+
   const $table = $($(selector).get(index))
   const rows = $table.find('tbody tr').toArray()
   return rows.map(row => {
     const cells = $(row).find('td')
-    const caseNo = parseInt($(cells[0]).text())
-    const location = $(cells[1]).text()
+    const date = $(cells[0]).text()
+    const gender = $(cells[1]).text()
     const age = $(cells[2]).text()
-    const gender = $(cells[3]).text()
-    const details = $(cells[4]).text()
+    const location = $(cells[3]).text()
+    const overseas = $(cells[4]).text()
     return {
-      caseNo,
-      location: location.trim(),
-      // Fix up the age groups, where actual age was provided i.e. 64 not 60s
-      age: (age !== 'Teens' && age !== 'Child' && !age.includes('0s')
-        ? `${age[0]}0s`
-        : age
-      ).trim(),
-      gender: gender.trim(),
-      details,
+      date: parseDate(date),
+      location: parseLocation(location),
+      age: parseAge(age),
+      gender: parseGender(gender),
+      overseas: parseOverseas(overseas),
     }
   })
 }
--- setvars.sh
+++ setvars.sh
@@ -1,6 +1,6 @@
 firebase functions:config:set \
   scraper.sourceurl="https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-cases/covid-19-current-cases-details" \
   scraper.tables.confirmed.selector=".table-style-two" \
-  scraper.tables.confirmed.index="0" \
+  scraper.tables.confirmed.index="1" \
   scraper.tables.probable.selector=".table-style-two" \
-  scraper.tables.probable.index="1"
+  scraper.tables.probable.index="2"
